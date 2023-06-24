df4bd6c1d482a0e09ce566b9036d0e4405226e4d
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-06-24 20:44:19 +0700

    init redisproj

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
-	"time"
+	"fmt"
+	"goredis/repositories"
 
-	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 
-	app := fiber.New()
+	db := initDatabase()
 
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		time.Sleep(10 * time.Millisecond)
-		return c.SendString("Hello World")
-	})
-	app.Listen(":8000")
+	productRepo := repositories.NewProductRepositoeyDB(db)
 
+	products, err := productRepo.GetProducts()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(products)
+
+	// app := fiber.New()
+
+	// app.Get("/hello", func(c *fiber.Ctx) error {
+	// 	time.Sleep(10 * time.Millisecond)
+	// 	return c.SendString("Hello World")
+	// })
+	// app.Listen(":8000")
+
+}
+
+func initDatabase() *gorm.DB {
+	dial := mysql.Open("root:Bossza555@tcp(localhost:3306)/redistest")
+	db, err := gorm.Open(dial, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
